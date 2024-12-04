1af431749954d3898615e1adf197dc307383fdfd
andreinapazz <108377491+andreinapazz@users.noreply.github.com>
2024-12-04 01:58:29 +0000

    Terminado

--- src/models.py
+++ src/models.py
@@ -7,24 +7,57 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    lastName = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False, unique= True)
+    userName = Column(String(250), nullable=False, unique= True)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Post(Base):
+    __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    likes = Column(Integer, nullable = True)
+    image_id= Column(Integer, ForeignKey('image.id'))
+    user_id= Column(Integer, ForeignKey('user.id'))
+    
+    user = relationship(User)
 
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    comment_content = Column(String(350))
+    
+    post = relationship(Post)
+
+
+class Image(Base):
+    __tablename__= 'image'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    
+    post = relationship(Post)
+
+class Curso(Base):
+    __tablename__= 'curso'
+    id = Column(Integer, primary_key=True)
+    nameCurso = Column(String(250), nullable=False)
+    priceCurso = Column(String(250), nullable=False)
+
+class Pertenece(Base):
+    __tablename__= 'pertenece'
+    id = Column(Integer, primary_key=True)
+    curso_id = Column(Integer, ForeignKey('curso.id'))
+    user_id= Column(Integer, ForeignKey('user.id'))
+
+    user = relationship(User)
+    curso = relationship(Curso)
+
+   
     def to_dict(self):
         return {}
 
